56beb6ae9651af05f813bb7594f4c013975df363
amor <niuxiaobo_5552@163.com>
2016-08-01 14:17:56 +0800

    增加控制双屏的类，对联网进行进一步的优化

---
+++ app/src/main/java/jiemi/com/base/network/Network.java
@@ -0,0 +1,16 @@
+package jiemi.com.base.network;
+
+/**
+ * 类名：网络接口
+ * <p/>
+ * 描述：服务器接口
+ *
+ * @author：NIU Date：2016/8/1
+ */
+public  interface Network {
+    String BASE="";
+    /**
+     * 获取双屏广告页的接口
+     */
+    String PAGER_VIEW=BASE+"";
+}
---
+++ app/src/main/java/jiemi/com/base/widget/LauncherSecondScreen.java
@@ -0,0 +1,159 @@
+package jiemi.com.base.widget;
+
+
+
+
+
+import java.util.ArrayList;
+
+
+import android.annotation.SuppressLint;
+import android.app.Presentation;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.view.Display;
+import android.view.View;
+
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import jiemi.com.base.R;
+
+@SuppressLint("NewApi")
+public class LauncherSecondScreen extends Presentation {
+	 
+	    private ViewPager pager;
+	    private int mCurrentItem = 0;
+	    private int mItem;
+	    private Runnable mPagerAction;
+	    private boolean isFrist = true;
+	    private int[] imgResIDs = new int[]
+	    	    { R.drawable.a, R.drawable.b, R.drawable.c, R.drawable.d, R.drawable.e };
+	    private ArrayList<View> items = new ArrayList<View>();
+	@SuppressLint("NewApi")
+	public LauncherSecondScreen(Context outerContext, Display display) {
+		super(outerContext, display);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		 setContentView(R.layout.dialog_second_screen_content);
+		 pager = (ViewPager) findViewById(R.id.tuijian_pager);
+		initAllItems();
+		initView();
+	}
+	private void initView(){
+		 pager.setAdapter(new PagerAdapter()
+	        {
+
+	            @Override
+	            public Object instantiateItem(View container, int position)
+	            {
+	                View layout = items.get(position % items.size());
+	                pager.addView(layout);
+	                return layout;
+	            }
+	            
+
+	            @Override
+	            public void destroyItem(View container, int position, Object object)
+	            {
+	                View layout = items.get(position % items.size());
+	                pager.removeView(layout);
+	            }
+	            
+	            @Override
+	            public boolean isViewFromObject(View arg0, Object arg1)
+	            {
+	                return arg0 == arg1;
+	                
+	            }
+	            
+	            @Override
+	            public int getCount()
+	            {
+	                return imgResIDs.length;
+	            }
+	            
+	        });
+	        pager.setOnPageChangeListener(new OnPageChangeListener()
+	        {
+	            
+	            @Override
+	            public void onPageSelected(final int arg0)
+	            {
+	                mCurrentItem = arg0 % items.size();
+	                pager.setCurrentItem(mCurrentItem);
+	            
+	            }
+	            
+	            @Override
+	            public void onPageScrolled(int arg0, float arg1, int arg2)
+	            {
+	                // TODO Auto-generated method stub
+	            }
+	            
+	            @Override
+	            public void onPageScrollStateChanged(int arg0)
+	            {
+	                // TODO Auto-generated method stub
+	                
+	            }
+	        });
+	        mPagerAction = new Runnable()
+	        {
+	            @Override
+	            public void run()
+	            {
+	                
+	                if (mItem != 0)
+	                {
+	                    if (isFrist == true)
+	                    {
+	                        mCurrentItem = 0;
+	                        isFrist = false;
+	                    }
+	                    else
+	                    {
+	                        if (mCurrentItem == items.size() - 1)
+	                        {
+	                            mCurrentItem = 0;
+	                        }
+	                        else
+	                        {
+	                            mCurrentItem++;
+	                        }
+	                    }
+	                    
+	                    pager.setCurrentItem(mCurrentItem);
+	                   
+	                    
+	                }
+	                pager.postDelayed(mPagerAction, 2500);
+	            }
+	        };
+	        pager.postDelayed(mPagerAction, 100);
+	}
+	private void initAllItems()
+    {
+
+        for (int i = 0; i < imgResIDs.length; i++)
+        {
+            items.add(initPagerItem(imgResIDs[i]));
+        }
+        mItem = items.size();
+    }
+	 private View initPagerItem(int resID)
+	    {
+	        View layout1 = getLayoutInflater().inflate(R.layout.item_advertise, null);
+	        ImageView imageView1 = (ImageView) layout1.findViewById(R.id.header_img);
+	        imageView1.setImageResource(resID);
+	        return layout1;
+	    }
+}
